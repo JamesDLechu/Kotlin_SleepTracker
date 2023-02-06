a619e26ade3dc967c193fdeb1820d4775824c1c8
JamesDLechu <jamesdlechu@gmail.com>
2023-02-06 17:07:03 -0600

    Update: LinearLayout replaced by GridLayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.SleepNightAdapter
 import com.example.android.trackmysleepquality.database.SleepDatabase
@@ -64,6 +65,9 @@ class SleepTrackerFragment : Fragment() {
 
         val adapter= SleepNightAdapter()
         binding.sleepList.adapter= adapter
+
+        val layoutManager= GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager= layoutManager
 
         viewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
